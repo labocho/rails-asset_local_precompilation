2e19414d0ed6d35c3bc6f895785028924cba7926
labocho <labocho@penguinlab.jp>
2019-11-01 13:54:34 +0900

    install ckeditor in test app

--- test/dummy/config/initializers/assets.rb
+++ test/dummy/config/initializers/assets.rb
@@ -12,3 +12,8 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w(application.js)
+Rails.application.config.assets.precompile += %w(
+  ckeditor/application.css
+  ckeditor/application.js
+  ckeditor/config.js
+)
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :notes
+  mount Ckeditor::Engine => "/ckeditor"
   root to: "home#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
